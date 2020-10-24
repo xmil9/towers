05f87fce6be9687bf906507063bfccfe24a853b4
Mike Lindner <mlindner9@yahoo.com>
2020-10-23 22:44:12 -0400

    Update essentutils.

--- deps/essentutils/type_traits_util.h
+++ deps/essentutils/type_traits_util.h
@@ -14,16 +14,36 @@ namespace sutil
 {
 ///////////////////
 
-template <typename T, typename = void> inline constexpr bool IsIterator_v = false;
+template <typename T, typename = void> inline constexpr bool isIterator_v = false;
 
 // Will only pass SFINAE, if the given type defines iterator_category.
 template <typename T>
 inline constexpr bool
-   IsIterator_v<T, std::void_t<typename std::iterator_traits<T>::iterator_category>> =
+   isIterator_v<T, std::void_t<typename std::iterator_traits<T>::iterator_category>> =
       true;
 
-template <typename T> struct IsIterator : std::bool_constant<IsIterator_v<T>>
+template <typename T> struct IsIterator : std::bool_constant<isIterator_v<T>>
 {
 };
+
+
+///////////////////
+
+template <typename T, typename... Types> struct IsOneOf : std::false_type
+{
+};
+
+template <typename T, typename U> struct IsOneOf<T, U> : std::is_same<T, U>
+{
+};
+
+template <typename T, typename U, typename... Rest>
+struct IsOneOf<T, U, Rest...>
+: std::integral_constant<bool, IsOneOf<T, U>::value || IsOneOf<T, Rest...>::value>
+{
+};
+
+template <typename T, typename... Types>
+inline constexpr bool isOneOf_v = IsOneOf<T, Types...>::value;
 
 } // namespace sutil
